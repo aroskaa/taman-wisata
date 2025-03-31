b39c08e9893c8c40a787ee63b4647e3d5ef067a9
aroskaa <divaaroska10@gmail.com>
2025-03-31 21:58:36 +0800

    fresh

---
+++ app/Http/Controllers/HeroSliderController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HeroSlider;
+use Illuminate\Http\Request;
+
+class HeroSliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HeroSlider $heroSlider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HeroSlider $heroSlider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HeroSlider $heroSlider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HeroSlider $heroSlider)
+    {
+        //
+    }
+}
